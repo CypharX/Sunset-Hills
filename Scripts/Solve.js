7c2ffdf8678348b271a0b21c94d96aaf54a6043b
Anthony D <CypharX@gmail.com>
2020-10-09 03:33:27 -0400

    code page added

--- Scripts/Solve.js
+++ Scripts/Solve.js
@@ -1,35 +1,46 @@
-﻿document.getElementById('sunBtn').addEventListener('click', function () {
-
+﻿//Wires event listener to call a function when the "sunBtn" button is clicked.
+document.getElementById('sunBtn').addEventListener('click', function () {
+    //Creates 5 variables and sets each one equal to the corresponding user input.
     let height1 = parseInt(document.getElementById('sunInput1').value);
     let height2 = parseInt(document.getElementById('sunInput2').value);
     let height3 = parseInt(document.getElementById('sunInput3').value);
     let height4 = parseInt(document.getElementById('sunInput4').value);
     let height5 = parseInt(document.getElementById('sunInput5').value);
-
+    //Creates a new array "heightArray" and puts the 5 inputs into it.
     let heightArray = new Array();
     heightArray.push(height1);
     heightArray.push(height2);
     heightArray.push(height3);
     heightArray.push(height4);
     heightArray.push(height5);
-
+    //Creates 2 new arrays. One to track if each building is taller than
+    //all previous buildings, and one to track the heights of all buildings
+    //that are taller than all previous bulidings.
     let viewArray = ['true'];
     let viewBldg = height1;
-    let tracker = 1;
     let trackerArray = [height1];
-
+    //Declaring a variable to track the number of buildings that can "see the sun".
+    let tracker = 1;
+    //Sets up a for loop to run 4 times, once for buildings 2-5.
     for (let i = 1; i <= 4; i++) {
+        //If the current building is tall than all previous buildings this will
+        //update the "viewBldg" variable to compare future values to, push true
+        //to the "viewArray", push the current value to the "trackerArray", and
+        //increment the tracker variable.
         if (heightArray[i] > viewBldg) {
             viewBldg = heightArray[i];
             viewArray.push('true');
             tracker++;
             trackerArray.push(heightArray[i])
         }
+        //If the current building is not taller than all previous buildings it will
+        //just push false to the "viewArray".
         else {
             viewArray.push('false')
         }
     }
-
+    //A series of "if else" functions that use the "viewArray" to return outputs that tell the user
+    //whether or the building can see the sun, the height of that building, and a check or x icon. 
     if (viewArray[0] == 'true') {
         document.getElementById('viewCheck1').innerHTML = `<span>Building 1 has a height of ${height1} and can see the sun. </span><img src="Images/Checkmark.png" />`;
     } else {
@@ -67,7 +78,7 @@
     }
 })
 
-//Function to listen for keypresses in "input1" and ignore all non-number characters
+//Function to listen for keypresses in "sunInput1" and ignore all non-number characters
 document.getElementById('sunInput1').addEventListener('keypress', function (evt) {
     //Creates the "char" variable and sets it equal to the character code of the 
     //pressed input
@@ -83,7 +94,7 @@ document.getElementById('sunInput1').addEventListener('keypress', function (evt)
         return false;
     }
 });
-//Same as above function but for "input2"
+//Same as above function but for "sunInput2"
 document.getElementById('sunInput2').addEventListener('keypress', function (evt) {
     let char = evt.which;
     if (char >= 48 && char <= 57) {
@@ -94,14 +105,9 @@ document.getElementById('sunInput2').addEventListener('keypress', function (evt)
         return false;
     }
 });
-//Function to listen for keypresses in "input1" and ignore all non-number characters
-document.getElementById('sunInput3').addEventListener('keypress', function (evt) {
-    //Creates the "char" variable and sets it equal to the character code of the 
-    //pressed input
-    let char = evt.which;
-    //Uses the value of "char" to determine the given character using its Ascii
-    // code and returning true if it's a character we want and false if it's any
-    //other character.
+//Same as above functions but for "sunInput3"
+document.getElementById('sunInput3').addEventListener('keypress', function (evt) {   
+    let char = evt.which;    
     if (char >= 48 && char <= 57) {
         return true;
     }
@@ -110,7 +116,7 @@ document.getElementById('sunInput3').addEventListener('keypress', function (evt)
         return false;
     }
 });
-//Same as above function but for "input2"
+//Same as above functions but for "sunInput4"
 document.getElementById('sunInput4').addEventListener('keypress', function (evt) {
     let char = evt.which;
     if (char >= 48 && char <= 57) {
@@ -121,6 +127,7 @@ document.getElementById('sunInput4').addEventListener('keypress', function (evt)
         return false;
     }
 });
+//Same as above functions but for "sunInput5"
 document.getElementById('sunInput5').addEventListener('keypress', function (evt) {
     let char = evt.which;
     if (char >= 48 && char <= 57) {
